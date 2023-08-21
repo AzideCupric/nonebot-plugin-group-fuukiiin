831495ab64c6107d993b0775924f4394a09e3ae2
Azide <rukuy@qq.com>
2023-08-21 23:12:11 +0800

    :bug: 修复错误的权限比较方法

--- src/plugins/nonebot_plugin_group_fuukiiin/repeat_fuukiiin.py
+++ src/plugins/nonebot_plugin_group_fuukiiin/repeat_fuukiiin.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from collections import Counter
 
 from expiringdict import ExpiringDict
 from nonebot import on_message
@@ -10,13 +10,6 @@ from nonebot.adapters.onebot.v11 import (
     MessageSegment,
 )
 from nonebot.log import logger
-
-
-@dataclass
-class RepeatRecord:
-    message: str
-    repeat_count: int
-
 
 MAX_REPEAT_COUNT = 3
 
@@ -31,35 +24,38 @@ async def repeat_fuukiiin(bot: Bot, event: GroupMessageEvent):
 
     stripped_msg = event.get_plaintext().strip()
     if event.user_id not in repeated_dict:
-        logger.debug(f"风纪委员检查到{event.user_id}第一次发送消息: {stripped_msg}，记录中...")
-        repeated_dict[event.user_id] = RepeatRecord(stripped_msg, 1)
+        logger.debug(f"风纪委员首次检查到{event.user_id}发送消息: {stripped_msg}，记录中...")
+        repeated_dict[event.user_id] = Counter([stripped_msg])
         await fuukiiin.finish()
 
-    if record := repeated_dict.get(event.user_id):
-        logger.debug("风纪委员检查到重复消息，检查是否超过最大重复次数...")
-        assert isinstance(record, RepeatRecord)
-        if record.message == stripped_msg:
-            repeated_dict[event.user_id].repeat_count += 1  # type: ignore
-            logger.debug(
-                f"风纪委员检查到重复消息:{record.message}，重复次数+1，当前重复次数为:{record.repeat_count}次!"
-            )
-            if record.repeat_count > MAX_REPEAT_COUNT:
-                logger.debug(f"风纪委员检查到重复消息超过{MAX_REPEAT_COUNT}次，检查发送者身份...")
-                if event.sender.role == GROUP_MEMBER:
-                    logger.debug("发送者是群员，处以禁言5分钟，通知发送者")
-                    await bot.set_group_ban(
-                        group_id=event.group_id, user_id=event.user_id, duration=60 * 5
-                    )
-                    await fuukiiin.finish(
-                        MessageSegment.at(event.user_id)
-                        + f"检测到重复消息{MAX_REPEAT_COUNT}次，已禁言"
-                    )
-                else:
-                    logger.debug("发送者是管理员，不处以禁言，仅通知发送者")
-                    await fuukiiin.finish("检测到管理员发送重复消息，警告")
+    record = repeated_dict.get(event.user_id)  # 不会取出 "", 因为是一个Counter类的实例
+    logger.debug("风纪委员检查到user_id在记录中，检查消息是否重复...")
+    assert isinstance(record, Counter)
+    record.update([stripped_msg])
+    most_common_message, most_common_count = record.most_common(1)[0]
+    if most_common_count > MAX_REPEAT_COUNT:
+        logger.debug(
+            f"风纪委员检查到重复消息{most_common_message}超过{MAX_REPEAT_COUNT}次，检查发送者身份..."
+        )
 
-            logger.debug("消息重复次数未超过最大重复次数，风纪委员进行下一轮巡逻")
-            await fuukiiin.finish()
+        match event.sender.role:
+            case "admin":
+                logger.debug("发送者是管理员，不处以禁言，仅通知发送者")
+                record.pop(most_common_message)
+                await fuukiiin.finish("检测到管理员发送重复消息，警告")
+            case "member":
+                logger.debug("发送者是群员，处以禁言5分钟，通知发送者")
+                await bot.set_group_ban(
+                    group_id=event.group_id, user_id=event.user_id, duration=60 * 5
+                )
+                record.pop(most_common_message)
+                await fuukiiin.finish(
+                    MessageSegment.at(event.user_id) + f"检测到重复消息{MAX_REPEAT_COUNT}次，已禁言"
+                )
+            case _:
+                logger.debug("发送者身份未知，不处以禁言，仅通知发送者")
+                record.pop(most_common_message)
+                await fuukiiin.finish("检测到未知成员发送重复消息，警告")
 
-    logger.debug("风纪委员未检查到重复消息，下一轮巡逻...")
+    logger.debug("风纪委员检查到重复消息未超过阈值，不做处理，继续巡逻...")
     await fuukiiin.finish()
