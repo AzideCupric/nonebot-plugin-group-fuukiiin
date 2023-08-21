3adb37d9de5b321da6b88fe6bf5c4fbd93ed2a21
Azide <rukuy@qq.com>
2023-08-21 22:42:51 +0800

    :bug: 在__init__声明

--- src/plugins/nonebot_plugin_group_fuukiiin/__init__.py
+++ src/plugins/nonebot_plugin_group_fuukiiin/__init__.py
@@ -1,7 +1,6 @@
-from nonebot import get_driver
-
-from . import pinyin_fuukiiin
-from .config import Config
+from . import pinyin_fuukiiin as pinyin_fuukiiin
+from . import repeat_fuukiiin as repeat_fuukiiin
+from .config import Config as Config
 
 # Export something for other plugin
 # export = nonebot.export()
